5977457a294ac427e745f2f6136c06086a023123
Ryan Hartlage <ryanplusplus@gmail.com>
2023-03-26 15:31:41 -0400

    [New practice exercise]: Sublist (#382)
    * Skeleton and spec template
    
    * Added example
    
    * Simplified example

---
+++ exercises/practice/sublist/src/sublist.cr
@@ -0,0 +1,12 @@
+module Sublist
+  enum Classification
+    Equal
+    Unequal
+    Sublist
+    Superlist
+  end
+
+  def self.classify(list1 : Array(Int32), list2 : Array(Int32)) : Classification
+    # Write your code for the 'Sublist' exercise in this file.
+  end
+end
