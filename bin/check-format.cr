bb22e2227230b65e6854adac5f242eaef3a51360
meatball <69751659+meatball133@users.noreply.github.com>
2023-04-25 11:26:14 +0200

    [Ci]: Add so ci looks in the json file for path (#408)
    * Fixed check-formatter
    
    * Fix
    
    * Fixed path
    
    * Fix so it doesnt include it as an array
    
    * Fixed path
    
    * Add message for when it is formated for concept

--- bin/check-format.cr
+++ bin/check-format.cr
@@ -1,47 +1,49 @@
+require "json"
+
 module Check_template
   def self.check_practice_exercises
-    Dir.open("./exercises/practice").each do |dir|
-      unless dir == "." || dir == ".."
-        File.write("/tmp/example.cr", File.open("./exercises/practice/#{dir}/.meta/src/example.cr"))
-        File.write("/tmp/src.cr", File.open("./exercises/practice/#{dir}/src/#{dir.gsub("-", "_")}.cr"))
-        File.write("/tmp/spec.cr", File.open("./exercises/practice/#{dir}/spec/#{dir.gsub("-", "_")}_spec.cr"))
+    Dir.open("./exercises/practice").each_child do |dir|
+      json = JSON.parse(File.read("./exercises/practice/#{dir}/.meta/config.json"))
+      File.write("/tmp/example.cr", File.read("./exercises/practice/#{dir}/.meta/src/example.cr"))
+      File.write("/tmp/src.cr", File.read("./exercises/practice/#{dir}/#{json["files"]["solution"][0]}"))
+      File.write("/tmp/spec.cr", File.read("./exercises/practice/#{dir}/#{json["files"]["test"][0]}"))
 
-        system("crystal tool format /tmp/example.cr")
-        system("crystal tool format /tmp/src.cr")
-        system("crystal tool format /tmp/spec.cr")
-        case
-        when !File.same_content?("/tmp/example.cr", "./exercises/practice/#{dir}/.meta/src/example.cr")
-          raise "The example file \"./exercises/practice/#{dir}/.meta/src/example.cr\" is not formatted"
-        when !File.same_content?("/tmp/src.cr", "./exercises/practice/#{dir}/src/#{dir.gsub("-", "_")}.cr")
-          raise "The source file \"./exercises/practice/#{dir}/src/#{dir.gsub("-", "_")}.cr\" is not formatted"
-        when !File.same_content?("/tmp/spec.cr", "./exercises/practice/#{dir}/spec/#{dir.gsub("-", "_")}_spec.cr")
-          raise "The spec file \"./exercises/practice/#{dir}/spec/#{dir.gsub("-", "_")}_spec.cr\" is not formatted"
-        else
-          puts "All files in \"./exercises/practice/#{dir}\" are formatted"
-        end
+      system("crystal tool format /tmp/example.cr")
+      system("crystal tool format /tmp/src.cr")
+      system("crystal tool format /tmp/spec.cr")
+      case
+      when !File.same_content?("/tmp/example.cr", "./exercises/practice/#{dir}/.meta/src/example.cr")
+        raise "The example file \"./exercises/practice/#{dir}/.meta/src/example.cr\" is not formatted"
+      when !File.same_content?("/tmp/src.cr", "./exercises/practice/#{dir}/#{json["files"]["solution"][0]}")
+        raise "The source file \"./exercises/practice/#{dir}/#{json["files"]["solution"]}\" is not formatted"
+      when !File.same_content?("/tmp/spec.cr", "./exercises/practice/#{dir}/#{json["files"]["test"][0]}")
+        raise "The spec file \"./exercises/practice/#{dir}/#{json["files"]["test"]}\" is not formatted"
+      else
+        puts "All files in \"./exercises/practice/#{dir}\" are formatted"
       end
     end
   end
 
   def self.check_concept_exercises
     if Dir.exists?("exercises/concept")
-      Dir.open("./exercises/concept").each do |dir|
-        unless dir == "." || dir == ".."
-          File.write("/tmp/exemplar.cr", File.open("./exercises/concept/#{dir}/.meta/src/exemplar.cr"))
-          File.write("/tmp/src.cr", File.open("./exercises/concept/#{dir}/src/#{dir.gsub("-", "_")}.cr"))
-          File.write("/tmp/spec.cr", File.open("./exercises/concept/#{dir}/spec/#{dir.gsub("-", "_")}_spec.cr"))
+      Dir.open("./exercises/concept").each_child do |dir|
+        json = JSON.parse(File.read("./exercises/concept/#{dir}/.meta/config.json"))
+        File.write("/tmp/exemplar.cr", File.read("./exercises/concept/#{dir}/.meta/src/exemplar.cr"))
+        File.write("/tmp/src.cr", File.read("./exercises/concept/#{dir}/#{json["files"]["solution"][0]}"))
+        File.write("/tmp/spec.cr", File.read("./exercises/concept/#{dir}/#{json["files"]["test"][0]}"))
 
-          system("crystal tool format /tmp/exemplar.cr")
-          system("crystal tool format /tmp/src.cr")
-          system("crystal tool format /tmp/spec.cr")
-          case
-          when !File.same_content?("/tmp/exemplar.cr", "./exercises/concept/#{dir}/.meta/src/exemplar.cr")
-            raise "The exemplar file \"./exercises/concept/#{dir}/.meta/src/exemplar.cr\" is not formatted"
-          when !File.same_content?("/tmp/src.cr", "./exercises/concept/#{dir}/src/#{dir.gsub("-", "_")}.cr")
-            raise "The source file \"./exercises/concept/#{dir}/src/#{dir.gsub("-", "_")}.cr\" is not formatted"
-          when !File.same_content?("/tmp/spec.cr", "./exercises/concept/#{dir}/spec/#{dir.gsub("-", "_")}_spec.cr")
-            raise "The spec file \"./exercises/concept/#{dir}/spec/#{dir.gsub("-", "_")}_spec.cr\" is not formatted"
-          end
+        system("crystal tool format /tmp/exemplar.cr")
+        system("crystal tool format /tmp/src.cr")
+        system("crystal tool format /tmp/spec.cr")
+        case
+        when !File.same_content?("/tmp/exemplar.cr", "./exercises/concept/#{dir}/.meta/src/exemplar.cr")
+          raise "The exemplar file \"./exercises/concept/#{dir}/.meta/src/exemplar.cr\" is not formatted"
+        when !File.same_content?("/tmp/src.cr", "./exercises/concept/#{dir}/#{json["files"]["solution"][0]}")
+          raise "The source file \"./exercises/concept/#{dir}/#{json["files"]["solution"]}\" is not formatted"
+        when !File.same_content?("/tmp/spec.cr", "./exercises/concept/#{dir}/#{json["files"]["test"][0]}")
+          raise "The spec file \"./exercises/concept/#{dir}/#{json["files"]["test"]}\" is not formatted"
+        else
+          puts "All files in \"./exercises/concept/#{dir}\" are formatted"
         end
       end
     end
