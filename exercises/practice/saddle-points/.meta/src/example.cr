aeff2c4ad257e3428dbd33b14483bd4e1c31fff9
Carl <meatballsofmeatballs@gmail.com>
2023-06-21 15:47:59 +0200

    Add saddle point

---
+++ exercises/practice/saddle-points/.meta/src/example.cr
@@ -0,0 +1,13 @@
+module SaddlePoints
+  def self.saddle_points(matrix : Array(Array(Int32))) : Array(NamedTuple(row: Int32, column: Int32))
+    result = Array(NamedTuple(row: Int32, column: Int32)).new
+    matrix.each_with_index do |row, i|
+      row.each_with_index do |value, j|
+        if value == row.max && value == matrix.transpose[j].min
+          result << {row: i + 1, column: j + 1}
+        end
+      end
+    end
+    result
+  end
+end
