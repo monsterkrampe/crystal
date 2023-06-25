de123b0fd0572e26a31b5b9be4800483c7f28ef1
Ryan Hartlage <ryanplusplus@gmail.com>
2023-06-25 10:35:58 -0400

    Return Set from SaddlePoints.saddle_points (#429)

--- exercises/practice/saddle-points/.meta/src/example.cr
+++ exercises/practice/saddle-points/.meta/src/example.cr
@@ -1,6 +1,6 @@
 module SaddlePoints
-  def self.saddle_points(matrix : Array(Array(Int32))) : Array(NamedTuple(row: Int32, column: Int32))
-    result = Array(NamedTuple(row: Int32, column: Int32)).new
+  def self.saddle_points(matrix : Array(Array(Int32))) : Set(NamedTuple(row: Int32, column: Int32))
+    result = Set(NamedTuple(row: Int32, column: Int32)).new
     matrix.each_with_index do |row, i|
       row.each_with_index do |value, j|
         if value == row.max && value == matrix.transpose[j].min
--- exercises/practice/saddle-points/src/saddle_points.cr
+++ exercises/practice/saddle-points/src/saddle_points.cr
@@ -1,5 +1,5 @@
 module SaddlePoints
-  def self.saddle_points(matrix : Array(Array(Int32))) : Array(NamedTuple(row: Int32, column: Int32))
+  def self.saddle_points(matrix : Array(Array(Int32))) : Set(NamedTuple(row: Int32, column: Int32))
     # Write your code for the 'SaddlePoints' exercise in this file.
   end
 end
