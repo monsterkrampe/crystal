9021f684ae202e38aa51dd7896f23a9b0d63a607
meatball <69751659+meatball133@users.noreply.github.com>
2023-08-16 09:47:11 +0200

    Simplify script (#467)

--- bin/check-format.cr
+++ bin/check-format.cr
@@ -1,49 +1,37 @@
 require "json"
+require "compiler/crystal/formatter"
 
 module Check_template
   def self.check_practice_exercises
-    Dir.open("./exercises/practice").each_child do |dir|
-      json = JSON.parse(File.read("./exercises/practice/#{dir}/.meta/config.json"))
-      File.write("/tmp/example.cr", File.read("./exercises/practice/#{dir}/.meta/src/example.cr"))
-      File.write("/tmp/src.cr", File.read("./exercises/practice/#{dir}/#{json["files"]["solution"][0]}"))
-      File.write("/tmp/spec.cr", File.read("./exercises/practice/#{dir}/#{json["files"]["test"][0]}"))
-
-      system("crystal tool format /tmp/example.cr")
-      system("crystal tool format /tmp/src.cr")
-      system("crystal tool format /tmp/spec.cr")
-      case
-      when !File.same_content?("/tmp/example.cr", "./exercises/practice/#{dir}/.meta/src/example.cr")
-        raise "The example file \"./exercises/practice/#{dir}/.meta/src/example.cr\" is not formatted"
-      when !File.same_content?("/tmp/src.cr", "./exercises/practice/#{dir}/#{json["files"]["solution"][0]}")
-        raise "The source file \"./exercises/practice/#{dir}/#{json["files"]["solution"]}\" is not formatted"
-      when !File.same_content?("/tmp/spec.cr", "./exercises/practice/#{dir}/#{json["files"]["test"][0]}")
-        raise "The spec file \"./exercises/practice/#{dir}/#{json["files"]["test"]}\" is not formatted"
-      else
-        puts "All files in \"./exercises/practice/#{dir}\" are formatted"
-      end
-    end
+    check_folder("./exercises/practice")
   end
 
   def self.check_concept_exercises
-    if Dir.exists?("exercises/concept")
-      Dir.open("./exercises/concept").each_child do |dir|
-        json = JSON.parse(File.read("./exercises/concept/#{dir}/.meta/config.json"))
-        File.write("/tmp/exemplar.cr", File.read("./exercises/concept/#{dir}/.meta/src/exemplar.cr"))
-        File.write("/tmp/src.cr", File.read("./exercises/concept/#{dir}/#{json["files"]["solution"][0]}"))
-        File.write("/tmp/spec.cr", File.read("./exercises/concept/#{dir}/#{json["files"]["test"][0]}"))
+    check_folder("./exercises/concept")
+  end
 
-        system("crystal tool format /tmp/exemplar.cr")
-        system("crystal tool format /tmp/src.cr")
-        system("crystal tool format /tmp/spec.cr")
-        case
-        when !File.same_content?("/tmp/exemplar.cr", "./exercises/concept/#{dir}/.meta/src/exemplar.cr")
-          raise "The exemplar file \"./exercises/concept/#{dir}/.meta/src/exemplar.cr\" is not formatted"
-        when !File.same_content?("/tmp/src.cr", "./exercises/concept/#{dir}/#{json["files"]["solution"][0]}")
-          raise "The source file \"./exercises/concept/#{dir}/#{json["files"]["solution"]}\" is not formatted"
-        when !File.same_content?("/tmp/spec.cr", "./exercises/concept/#{dir}/#{json["files"]["test"][0]}")
-          raise "The spec file \"./exercises/concept/#{dir}/#{json["files"]["test"]}\" is not formatted"
+  def self.check_folder(path)
+    if Dir.exists?(path)
+      Dir.open(path).each_child do |dir|
+        json = JSON.parse(File.read("#{path}/#{dir}/.meta/config.json"))
+
+        if json["files"]["exemplar"]?
+          exemple = File.read("#{path}/#{dir}/#{json["files"]["exemplar"][0]}")
         else
-          puts "All files in \"./exercises/concept/#{dir}\" are formatted"
+          exemple = File.read("#{path}/#{dir}/#{json["files"]["example"][0]}")
+        end
+        solution = File.read("#{path}/#{dir}/#{json["files"]["solution"][0]}")
+        spec = File.read("#{path}/#{dir}/#{json["files"]["test"][0]}")
+
+        case
+        when !(Crystal.format(exemple) == exemple)
+          raise "The example file \"#{path}/#{dir}/.meta/src/#{json["files"]["exemplar"]? ? "exemplar" : "exemple" }.cr\" is not formatted"
+        when !(Crystal.format(solution) == solution)
+          raise "The solution file \"#{path}/#{dir}/#{json["files"]["solution"]}\" is not formatted"
+        when !(Crystal.format(spec) == spec)
+          raise "The spec file \"#{path}/#{dir}/#{json["files"]["test"]}\" is not formatted"
+        else
+          puts "All files in \"#{path}/#{dir}\" are formatted"
         end
       end
     end
