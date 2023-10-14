0daff64a515b79a8a0a29808989e6a673c9aaf36
meatball <69751659+meatball133@users.noreply.github.com>
2023-10-14 23:28:16 +0200

    Simplify example (#524)

--- exercises/practice/pangram/.meta/src/example.cr
+++ exercises/practice/pangram/.meta/src/example.cr
@@ -2,6 +2,6 @@ class Pangram
   NUMBER_OF_ALPHA_CHARACTERS = 26
 
   def self.pangram?(input : String)
-    input.downcase.gsub(/[^a-z]/, "").chars.sort.uniq.size == NUMBER_OF_ALPHA_CHARACTERS
+    input.downcase.gsub(/[^a-z]/, "").chars.uniq.size == NUMBER_OF_ALPHA_CHARACTERS
   end
 end
