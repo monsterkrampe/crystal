d32438a3d9b4dd3ce0732bdfb40c2decb400b6d9
Kah Goh <villastar@yahoo.com.au>
2023-11-01 03:51:19 +0800

    Add Pop Count (#545)
    * Add Pop Count
    
    * Change Pop Count to practice bit-manipulation

---
+++ exercises/practice/pop-count/.meta/src/example.cr
@@ -0,0 +1,10 @@
+class PopCount
+  def self.egg_count(number : Int32) : Int32
+    count : Int32 = 0
+    while number > 0
+      count += (number & 1)
+      number = number >> 1
+    end
+    count
+  end
+end
