79824393a3912041dac89e9247a87f4063a2aeaf
Ryan Hartlage <ryanplusplus@gmail.com>
2023-11-19 11:21:44 -0500

    [Secrets]: Address Ameba feedback (#562)

--- exercises/concept/secrets/src/secrets.cr
+++ exercises/concept/secrets/src/secrets.cr
@@ -1,5 +1,5 @@
 module Secrets
-  def self.shift_back(value : UInt8, numberOfPlaces : UInt8) : UInt8
+  def self.shift_back(value : UInt8, number_of_places : UInt8) : UInt8
     raise "Please implement the Secrets.shift_back method"
   end
 
@@ -11,7 +11,7 @@ module Secrets
     raise "Please implement the Secrets.set_bits method"
   end
 
-  def self.reverse_xor(value : UInt8, agreedValue : UInt8) : UInt8
+  def self.reverse_xor(value : UInt8, agreed_value : UInt8) : UInt8
     raise "Please implement the Secrets.reverse_xor method"
   end
 end
